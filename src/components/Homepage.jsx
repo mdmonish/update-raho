bb99d8aa5ed9c877f8dfce518e92b4b5a62a0107
Mohammad Monish <66717066+mdmonish@users.noreply.github.com>
2025-08-12 16:30:08 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -60,6 +60,7 @@ const Homepage = () => {
       <button className="text-sky-600 text-md md:hidden mt-2 flex ml-auto">
         <Link to="/cryptocurrencies">Show More</Link>
       </button>
+{/*       News */}
 {/*       <div className="sm:flex justify-between mt-6 mb-4 text-lg md:text-2xl font-bold">
         <h2>Latest Crypto News</h2>
         <button className="text-sky-600 text-sm lg:text-lg hidden md:block">
